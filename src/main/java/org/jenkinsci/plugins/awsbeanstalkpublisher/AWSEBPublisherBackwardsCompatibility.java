2ffaeff65c36675d0f8494db59514dddf09ab815
Tim Jacomb <timjacomb1@gmail.com>
2026-08-03 14:26:22 +0100

    Migrate from Commons Lang 2 to Commons Lang 3

--- src/main/java/org/jenkinsci/plugins/awsbeanstalkpublisher/AWSEBPublisherBackwardsCompatibility.java
+++ src/main/java/org/jenkinsci/plugins/awsbeanstalkpublisher/AWSEBPublisherBackwardsCompatibility.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jenkinsci.plugins.awsbeanstalkpublisher.extensions.AWSEBElasticBeanstalkSetup;
 import org.jenkinsci.plugins.awsbeanstalkpublisher.extensions.AWSEBS3Setup;
 import org.jenkinsci.plugins.awsbeanstalkpublisher.extensions.envlookup.ByName;
